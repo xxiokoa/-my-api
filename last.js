5cab5f9f6de434fe9b190af2d22a3e31bfe96d71
xxiokoa <128106926+xxiokoa@users.noreply.github.com>
2023-03-18 08:28:55 +0800

    Create last.js

---
+++ last.js
@@ -0,0 +1,17 @@
+ axios.post(
+                "https://api.openai.com/v1/completions",
+                {
+                    prompt: '你好啊',
+                    max_tokens: 50,
+                    model: 'text-davinci-003',
+                },
+                {
+                    headers: {
+                        "content-type": "application/json",
+                        Authorization: 'Bearer ' + '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', // 将密钥作为Bearer Token传递
+                    },
+                }
+            )
+            .then((res) => {
+                console.log(res.data.choices[0].text);
+            })
